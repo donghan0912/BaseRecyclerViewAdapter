c5c5cbced48ff94cd361afd18ce27268726e6b99
董晗 <donghan2010@126.com>
2017-06-08 00:19:24 +0800

    添加瀑布流

--- sample/src/main/java/com/hpu/baserecyclerviewadapter/sample/StaggeredFragment.java
+++ sample/src/main/java/com/hpu/baserecyclerviewadapter/sample/StaggeredFragment.java
@@ -3,17 +3,15 @@ package com.hpu.baserecyclerviewadapter.sample;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.hpu.baserecyclerviewadapter.adapter.BaseMultiTypeAdapter;
 import com.hpu.baserecyclerviewadapter.item.BaseItem;
-import com.hpu.baserecyclerviewadapter.item.SimpleItem;
-import com.hpu.baserecyclerviewadapter.sample.multi.FirstItem;
 import com.hpu.baserecyclerviewadapter.sample.multi.FourthItem;
 
 import java.util.ArrayList;
@@ -24,7 +22,7 @@ import java.util.List;
  */
 
 public class StaggeredFragment extends Fragment {
-
+    // TODO 参考：http://blog.csdn.net/qq_28057577/article/details/54579320
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -36,11 +34,11 @@ public class StaggeredFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
         StaggeredGridLayoutManager manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
-
+        manager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
         recyclerView.setLayoutManager(manager);
         List<BaseItem> list = new ArrayList<>();
         for (int i = 0; i < 40; i++) {
-            list.add(new FourthItem());
+            list.add(new FourthItem(R.layout.layout_staggered));
         }
         final BaseMultiTypeAdapter adapter = new BaseMultiTypeAdapter(list);
         recyclerView.setAdapter(adapter);
@@ -49,5 +47,11 @@ public class StaggeredFragment extends Fragment {
 //        adapter.setExtraItem(new SimpleItem(R.layout.layout_loadmore));
 
 //        adapter.setStatusItem(new SimpleItem(R.layout.layout_loading));
+        adapter.setOnItemClickListener(new BaseMultiTypeAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- sample/src/main/java/com/hpu/baserecyclerviewadapter/sample/multi/FourthItem.java
+++ sample/src/main/java/com/hpu/baserecyclerviewadapter/sample/multi/FourthItem.java
@@ -1,13 +1,31 @@
 package com.hpu.baserecyclerviewadapter.sample.multi;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SizeReadyCallback;
 import com.hpu.baserecyclerviewadapter.holder.BaseViewHolder;
 import com.hpu.baserecyclerviewadapter.item.BaseItem;
+import com.hpu.baserecyclerviewadapter.sample.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2017/6/7.
  */
 
 public class FourthItem extends BaseItem {
+    private Map<Integer, Integer> map = new HashMap<>();
+    public FourthItem(int resource) {
+        super(resource);
+    }
 
     @Override
     public int getLayoutResource() {
@@ -16,6 +34,27 @@ public class FourthItem extends BaseItem {
 
     @Override
     public void onBindViewHolder(BaseViewHolder holder, int position) {
+
+
+
+        //设置图片的相对于屏幕的宽高比
+
+
+        ImageView img = holder.findViewById(R.id.img);
+        WindowManager wm = (WindowManager) img.getContext().getSystemService(Context.WINDOW_SERVICE);
+        Point point = new Point();
+        wm.getDefaultDisplay().getSize(point);
+        int width = point.x;
+        ViewGroup.LayoutParams params = img.getLayoutParams();
+        params.width = width/2;
+        if (position % 2 == 0) {
+            params.height = 500;
+        } else {
+            params.height = 700;
+        }
+        img.setLayoutParams(params);
+        Glide.with(img.getContext()).load(R.drawable.meinv).centerCrop().into(img);
+
 
     }
 }
